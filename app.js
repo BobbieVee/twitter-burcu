d7e9797b97c12a69493b4475a863aba2350964d3
Robert van der Mark <robert.vandermark@gmail.com>
2017-07-12 14:38:39 -0400

    nunjucks, baby!

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var express = require('express');
-var app = express();
 var chalk = require('chalk');
 var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+
+var app = express();
+
 
 var port = 3000 || process.env.PORT;
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true });
 
 app.use(morgan('combined'));
 
 app.get('/', function(req, res, next){
-	res.send('hello')
-})
+	res.render('index', {title: 'Hall of Fame', people: people})
+});
 
 app.use(function(req, res, next){
 	res.send('you idiot')
-})
+});
 
 app.listen(3000, function(){
 	console.log(chalk.blue(`Listening intently on port ${port}`));
-})
+});
