15e4a7fd4c5c1b6b0873d4f1924e9f59b7f8541f
Robert van der Mark <robert.vandermark@gmail.com>
2017-07-13 17:12:03 -0400

    front page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var chalk = require('chalk');
 var morgan = require('morgan');
 var nunjucks = require('nunjucks');
+const routes = require('./routes');
 
 var app = express();
 
@@ -10,15 +11,17 @@ var port = 3000 || process.env.PORT;
 
 const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
+app.use(express.static('public'));
+
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true });
 
 app.use(morgan('combined'));
 
-app.get('/', function(req, res, next){
-	res.render('index', {title: 'Hall of Fame', people: people})
-});
+app.use('/', routes);
+
+
 
 app.use(function(req, res, next){
 	res.send('you idiot')
